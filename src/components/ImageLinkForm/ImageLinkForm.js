b251fd49b471f591bdac77a94f189d6658a2d6d5
Erik <rkrettschlag@gmail.com>
2024-10-29 23:03:59 +0100

    minimal padding change

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = () => {
             {"This Mastermind will detect the faces in your pictures. Give it a try! (:s"}
          </p>
          <div className={'center'}>
-            <div className={'form center pa4 br3 shadow-3'}>
+            <div className={'form center pa3 br3 shadow-3'}>
                <input  className="f4 pa2 w-70 center" type="text"/>
                <button className='w-30 grow f4 link ph3 pv2 div white bg-light-purple'>Detect</button>
             </div>
