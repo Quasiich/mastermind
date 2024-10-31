b6853b5bdaa2de350be4f3e26bb9e42cc96438a8
Erik <rkrettschlag@gmail.com>
2024-10-31 23:10:29 +0100

    first babysteps with to connect the api

--- src/App.js
+++ src/App.js
@@ -4,18 +4,32 @@ import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import ParticlesBg from 'particles-bg';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-       <ParticlesBg className={'particles'} type="cobweb" num={150} bg={true}/>
-      <Navigation />
-      <Logo />
-       <Rank />
-       <ImageLinkForm />
-      {/*  <FaceRecognition />}*/}
-    </div>
-  );
+function App () {
+   const [input, setInput] = useState('');
+
+   const onInputChange = (event) => {
+      console.log(event.target.value);
+   }
+
+   const onButtonSubmit = () => {
+      console.log("click")
+   }
+
+   return(
+         <div className="App">
+            <ParticlesBg className={'particles'} type="cobweb" num={150} bg={true}/>
+            <Navigation/>
+            <Logo/>
+            <Rank/>
+            <ImageLinkForm
+               onInputChange={onInputChange}
+               onButtonSubmit={onButtonSubmit}/>
+            {/*  <FaceRecognition />}*/}
+         </div>
+   );
 }
+
 
 export default App;
