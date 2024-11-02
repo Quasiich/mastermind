3352bdb909e354cf7f35b75b3928bc3c7cf713af
Erik <48137656+Quasiich@users.noreply.github.com>
2024-11-02 18:06:17 +0100

    api request working now

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import ParticlesBg from 'particles-bg';
 import React, { Component} from "react";
-// import Clarifai from 'clarifai'
-//
-// const app = new Clarifai.App({
-//    apiKey: '94d0d6c8abab45ad9b7cba096ad295b9'
-// })
 
 const returnRequestOptions = (ImageUrl) => {
 
@@ -18,7 +13,7 @@ const returnRequestOptions = (ImageUrl) => {
    const USER_ID = 'quasiich';
    const APP_ID = 'my-first-application';
    // const MODEL_ID = 'face-detection';
-   const IMAGE_URL = {ImageUrl};
+   const IMAGE_URL = ImageUrl;
 
 
    const raw = JSON.stringify({
@@ -65,28 +60,7 @@ class App extends Component {
       this.setState({imageUrl: this.state.input})
       fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnRequestOptions(this.state.input))
          .then(response => response.json())
-         .then(result => {
-            const regions = result.outputs[0].data.regions;
-
-            regions.forEach(region => {
-               // Accessing and rounding the bounding box values
-               const boundingBox = region.region_info.bounding_box;
-               const topRow = boundingBox.top_row.toFixed(3);
-               const leftCol = boundingBox.left_col.toFixed(3);
-               const bottomRow = boundingBox.bottom_row.toFixed(3);
-               const rightCol = boundingBox.right_col.toFixed(3);
-
-               region.data.concepts.forEach(concept => {
-                  // Accessing and rounding the concept value
-                  const name = concept.name;
-                  const value = concept.value.toFixed(4);
-
-                  console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
-
-               });
-            });
-
-         })
+         .then(result => console.log(result))
          .catch(error => console.log('error', error));
    }
 
